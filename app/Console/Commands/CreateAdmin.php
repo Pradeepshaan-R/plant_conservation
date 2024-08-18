5a601182709c985c2e401f516beae8e14e94912e
Pradeepshaan Ramdas <pradeepshaan@akhitsolutions.com>
2024-08-18 14:17:29 +0530

    Admin command registered

---
+++ app/Console/Commands/CreateAdmin.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class CreateAdmin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'admin:create {email} {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new admin user';
+
+    /**
+     * Execute the console command.
+     */
+
+    public function handle()
+    {
+        $email = $this->argument('email');
+        $name = $this->argument('name');
+
+        $password = Str::random(10);
+
+        $admin = User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => Hash::make($password),
+            'role' => 'admin',
+        ]);
+
+        $this->info("Admin {$admin->name} created successfully. Password: {$password}");
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,11 +2,20 @@
 
 namespace App\Console;
 
+use App\Console\Commands\CreateAdmin;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        Commands\CreateAdmin::class,
+    ];
     /**
      * Define the application's command schedule.
      */
@@ -20,7 +29,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
